f71fb8aeb04b981fd888f9258f41fdefab01a37c
Pavanelli99 <pavanelli1999@gmail.com>
2022-08-10 19:43:36 -0400

    teste

--- src/heranca/AnimalTerrestre.java
+++ src/heranca/AnimalTerrestre.java
@@ -1,9 +1,29 @@
 package heranca;
 
+
 public class AnimalTerrestre {
 
     private String nome;
     private String raca;
     private int idade;
+
+   public int getIdade() {
+       return idade;
+   }
+   public void setIdade(int idade) {
+       this.idade = idade;
+   }
+   public String getNome() {
+       return nome;
+   }
+   public void setNome(String nome) {
+       this.nome = nome;
+   }
+   public String getRaca() {
+       return raca;
+   }
+   public void setRaca(String raca) {
+       this.raca = raca;
+   } 
     
 }
