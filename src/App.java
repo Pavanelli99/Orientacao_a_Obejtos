45d1f758c1c4241a887ffb6fbc50006bc9f86f2f
Pavanelli99 <pavanelli1999@gmail.com>
2022-08-10 21:47:38 -0400

    Aula_10_08_POO

--- src/App.java
+++ src/App.java
@@ -1,13 +1,14 @@
 import ex_ponto.Ponto;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Ponto p1 = new Ponto(2,7);
-        Ponto p2 = new Ponto(-2,3);
+         Ponto p1 = new Ponto(2,7);
+         Ponto p2 = new Ponto(-2,3);
 
-        double result = p1.distance(p1, p2);
+         double result = p1.distance(p1, p2);
 
-        System.out.println(result);
+         System.out.println(result);
 
     }
 }
---
+++ src/heranca/Funcionario.java
@@ -0,0 +1,21 @@
+package heranca;
+
+public class Funcionario extends Pessoa {
+    private int matricula;
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " matricula = '" + getMatricula() + "'" +
+            "}";
+    }
+
+}
